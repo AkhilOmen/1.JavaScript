ec5b10d019a6621f0d6e28f855cb229388011882
AkhilOmen <akhilbv333@gmail.com>
2021-12-16 22:04:08 +0530

    Added upto CustomeMap

---
+++ S S/Module_1/ArrayMap/CustomeMap.js
@@ -0,0 +1,20 @@
+Array.prototype.myMap = function(callback){
+
+    let rarr = [];
+
+    for( let i = 0; i < this.length; i++){
+        let val = this[i];
+        let rv = callback(val, i, this)
+        rarr.push(rv);
+    }
+
+    return rarr;
+
+}
+
+
+let arr = [1, 2, 3, 4, 5];
+let ans = arr.myMap(function(val, idx, arr){
+    return val * val;
+})
+console.log( "[ "+ ans + " ]" );
