e064b212c38835b7dd4d1f428926cb4434fa1a46
AkhilOmen <akhilbv333@gmail.com>
2021-12-21 23:26:26 +0530

    Added Upto reduce

--- S S/Java Script/8 Reduce/Questions/Q3.js
+++ S S/Java Script/8 Reduce/Questions/Q3.js
@@ -1,18 +1,21 @@
 // Union of array with no repeated values
 // this should be the output
-// ans = [2, 8, 10, 73, 34, 45, 31, 25, 64, 72, 88, 99]
+// ans = [2, 8, 10, 73, 34, 45, 31, 25, 64, 72, 88, 64, 99]
 
 let arr2 = [[2, 8, 10, 73], [34], [45, 31, 25, 10, 64, 72, 88], [64], [72, 88], [99, 34]];
 
 let arr = arr2.reduce( function(pv, cv) {
 
-    for(let i = 0; i < cv.length; i++){
-        if( pv.includes(cv[i]) == false ){
-            pv.push(cv[i]);
-        }
-    }
+    let temp = cv.filter(v => {
 
-    return pv;
+        if( pv.includes(v) ==  true){
+            return false;
+        }
+        return true;
+    })
+
+    return pv.concat(temp);
+    
 })
 
 console.log(arr);
--- S S/Java Script/8 Reduce/Questions/Q4.js
+++ S S/Java Script/8 Reduce/Questions/Q4.js
@@ -6,26 +6,9 @@ let arr2 = [[2, 8, 10, 73, 34], [34, 73, 71, 24], [45, 31, 25, 10, 64, 72, 88, 3
 
 let arr = arr2.reduce( (pv, cv, ci, oarr) => {
 
-    let flag = true;
-
-    for(let i = 0; i < cv.length; i++){
-
-        for( let j = 0; j < oarr.length; j++){
-
-            if( oarr[j].includes(cv[i]) == false ){
-                flag = false;
-            }
-
-        }
-
-        if( flag == true){
-            pv.push(cv[i]);
-        }
-
-    }
+    let temp = cv.filter( v => pv.includes(v));
+    return temp;
     
-    return pv;
-
-}, []); 
+}); 
 
 console.log(arr);
