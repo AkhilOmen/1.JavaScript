1e6e041978a6b810b412924aaf160c5b665c9633
AkhilOmen <akhilbv333@gmail.com>
2021-12-21 23:55:43 +0530

    Added Upto Reduce

--- S S/Java Script/8 Reduce/Questions/Q3.js
+++ S S/Java Script/8 Reduce/Questions/Q3.js
@@ -1,19 +1,12 @@
 // Union of array with no repeated values
 // this should be the output
-// ans = [2, 8, 10, 73, 34, 45, 31, 25, 64, 72, 88, 64, 99]
+// ans = [2, 8, 10, 73, 34, 45, 31, 25, 64, 72, 88, 99]
 
 let arr2 = [[2, 8, 10, 73], [34], [45, 31, 25, 10, 64, 72, 88], [64], [72, 88], [99, 34]];
 
 let arr = arr2.reduce( function(pv, cv) {
 
-    let temp = cv.filter(v => {
-
-        if( pv.includes(v) ==  true){
-            return false;
-        }
-        return true;
-    })
-
+    let temp = cv.filter(v => !pv.includes(v))
     return pv.concat(temp);
     
 })
--- S S/Java Script/8 Reduce/Questions/Q4.js
+++ S S/Java Script/8 Reduce/Questions/Q4.js
@@ -4,7 +4,7 @@
 
 let arr2 = [[2, 8, 10, 73, 34], [34, 73, 71, 24], [45, 31, 25, 10, 64, 72, 88, 34, 73], [64, 34, 73], [72, 88, 34, 73], [99, 34, 73]];
 
-let arr = arr2.reduce( (pv, cv, ci, oarr) => {
+let arr = arr2.reduce( (pv, cv) => {
 
     let temp = cv.filter( v => pv.includes(v));
     return temp;
