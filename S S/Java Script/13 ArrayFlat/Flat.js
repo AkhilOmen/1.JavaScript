3b475d9dc0ca3cdd875d5bda43c2ea9f4f9916df
AkhilOmen <akhilbv333@gmail.com>
2021-12-24 22:58:24 +0530

    Added Upto Custome Flat

--- S S/Java Script/13 ArrayFlat/Flat.js
+++ S S/Java Script/13 ArrayFlat/Flat.js
@@ -2,6 +2,7 @@ let arr = [10, 20, [50, 60, [90, 100]], 30, [70, [110, 120], 80], 40];
 
 
 // for flat(n) -> the arrays in that level and levels above will be busted and those elements will be attached to level 1  
+
 // For example -> 
 
 // for falt(1) -> The arrays in level 1 will be busted an the elements in the arr will be attached to level 1.
@@ -13,4 +14,12 @@ console.log(res1);
 // 2> arr.flat(2) => [ 10, 20, 50, 60, 90, 100, 30, 70, 110, 120, 80, 40 ]
 let res2 = arr.flat(2); 
 console.log(res2);
+
+// For n >= max levels, It will be equal to n - 1 i.e Here max level = 3, therefore flat(3) or flat(4) will be equal to flat(2)........
+let res3 = arr.flat(3); 
+console.log(res3);
+
+// Its will give use the same arr... i.e flat(0) will be equal to orginal array
+let res0 = arr.flat(0); 
+console.log(res0);
 
--- S S/Java Script/13 ArrayFlat/customeflat.js
+++ S S/Java Script/13 ArrayFlat/customeflat.js
@@ -1,6 +1,46 @@
 
 Array.prototype.myFlat = function(n){
 
-    
+    let ans = [];
+    let Qarr = this;
 
+    if( n == null ){
+        n = 1;
+    }
+
+    while( n != 0 ){ 
+        
+        Qarr.map( v => {
+
+            if( v.length == undefined || v.length == 1){
+                ans.push(v);
+            }
+            else{
+                v.map( v1 => {
+                    ans.push(v1);
+                })
+            }
+        })
+        
+        
+        if( Qarr.length == ans.length ){
+            Qarr = ans;
+            break;
+        }
+        
+        Qarr = ans;
+        ans = [];
+        n--;
+        
+    }
+    
+    return Qarr;
+    
 }
+
+// Question -> 
+let arr = [10, 20, [30, [50, [70, 80, 90], 60], 40], 100, [120, [150, [170, 180, 190], 160], 140, 200], 110];
+// let arr = ["a", "b", ["c", ["d", ["e","f", "g"], "h"], 40], 100, [120, [150, [170, 180, 190], 160], 140, 200], 110];
+
+let res1 = arr.myFlat(3);
+console.log(res1);
