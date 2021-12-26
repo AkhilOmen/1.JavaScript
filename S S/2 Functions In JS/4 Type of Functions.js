823d35e09c143d632614b4c001df84ad2ca9e33f
AkhilOmen <akhilbv333@gmail.com>
2021-12-26 18:26:02 +0530

    Added upto diff type of variables and functions

--- S S/2 Functions In JS/4 Type of Functions.js
+++ S S/2 Functions In JS/4 Type of Functions.js
@@ -1,42 +1,42 @@
 
 // 1. functions defination.
-// function fn(){
-//     console.log(" I am function defination ");
-// }
-// fn();
+function fn(){
+    console.log(" I am function defination ");
+}
+fn();
 
 
 // 2. function expression.
 // we can save a function address in a variable.
-// let secondFunction = function orginalFunction(){
-//     console.log(" I am expression");
-// }
-// // we can call the varibale, the function will run.
-// secondFunction();
+let secondFunction = function orginalFunction(){
+    console.log(" I am expression");
+}
+// we can call the varibale, the function will run.
+secondFunction();
 
 
 // 3. IIFEE -> Immediately invoke the function expression.
-// console.log("Before");
-// (function drawBoard(){
-//     console.log("board is Immerdiately drawn");
-// })(); 
-// console.log("After");
+console.log("Before");
+(function drawBoard(){
+    console.log("Board is Immerdiately drawn, I am IIFEE function");
+})(); 
+console.log("After");
 
 
 // 4. Anonymous functions
 // The functions that dosen't require names
 
 // // e1 -> for IIFEE function we don't need to give any name, as It runs automatically
-//      console.log("Before");
-//      (function (){
-//          console.log("I am Anonymous function");
-//      })(); 
-//      console.log("After");
+     console.log("Before");
+     (function (){
+         console.log("I am Anonymous function 1");
+     })(); 
+     console.log("After");
 
 // // e2 -> for function expression we don't need to give any name
-//      let secondFunction = function (){
-//          console.log( "I am Anonymous function" );
-//      }
-//      secondFunction();
+     let secondFunction1 = function (){
+         console.log("I am Anonymous function 2");
+     }
+     secondFunction1();
 
 
