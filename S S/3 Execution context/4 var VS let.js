139b0613550a9e61fd63313bbceb7a1412ec4a1b
AkhilOmen <akhilbv333@gmail.com>
2021-12-28 23:38:53 +0530

    Added upto var VS let

--- S S/3 Execution context/4 var VS let.js
+++ S S/3 Execution context/4 var VS let.js
@@ -16,24 +16,48 @@
     // You can't access the let varaible before declaration b/c It will in Temporal dead Zone
     
     // case - 1
-        let a;
-        console.log(a);
+        // let a;
+        // console.log(a);
         // undefined
         // It won't throw any error
     //
 
     // case - 2
-        console.log(b);
-        let b = 10;
+        // console.log(b);
+        // let b = 10;
         // It will throw an error -> Cannot access 'b' before initialization
     // 
 
     // case - 3
-        let c = 10;
-        console.log(c);
-        let c = 20; // It will throw an error ->  Identifier 'c' has already been declared
-        console.log(c);
+        // let c = 10;
+        // console.log(c);
+        // let c = 20; // It will throw an error ->  Identifier 'c' has already been declared
+        // console.log(c);
     // 
 
-// 
+//
 
+// Question ->
+
+    function fn(){
+        console.log("Before declaration", b);  
+        var b;
+        console.log("After declaration", b);    
+        b = 10;
+        if(true){
+            // It willnot throw an error even If we declare b again iwth let b/c we are using let in b/w block i.e {}
+            let b = 30;
+            console.log("After initialization", b);
+        }
+        console.log("After initialization", b);
+        
+    }
+    fn();
+
+    // Output
+    // Before declaration undefined
+    // After declaration undefined
+    // After initialization 30
+    // After initialization 10
+
+// 
