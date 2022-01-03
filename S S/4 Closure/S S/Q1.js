afe021b0aca1be94079542f2382b2b22064085fb
AkhilOmen <akhilbv333@gmail.com>
2022-01-03 22:07:19 +0530

    Closure questions

--- S S/4 Closure/S S/Q1.js
+++ S S/4 Closure/S S/Q1.js
@@ -1,6 +1,8 @@
 function powerCreator(obj){
 
+    // let val = obj.exp;
     let fun = function(base){
+        // let rv = Math.pow(base, val);
         let rv = Math.pow(base, obj.exp);
         return rv;
     }
@@ -20,3 +22,7 @@ console.log(val1);
 o1.exp = 3;
 let val2 = squarer(5);
 console.log(val2);
+
+o1.exp = 4;
+let val3 = squarer(5);
+console.log(val3);
